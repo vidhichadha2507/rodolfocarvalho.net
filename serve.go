c370d9c824c7ad7791a185fa983ac6101062caf6
Rodolfo Carvalho <rhcarvalho@gmail.com>
2013-10-22 05:20:55 +0800

    Simple static file server in Go for development.

---
+++ serve.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+)
+
+func main() {
+	addr := flag.String("addr", "localhost:8081", "host:port")
+	flag.Parse()
+	log.Printf("Serving at %s\n", *addr)
+	log.Fatal(http.ListenAndServe(*addr, http.FileServer(http.Dir("."))))
+}
